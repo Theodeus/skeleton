53f44e147bd8920f8c3bdb2d7a3e6ff6ec3e7e20
Oskar Eriksson <oskar.det@gmail.com>
2015-04-25 13:04:56 +0200

    Made sure jshint is executed and changed the grunt server task to be default instead

--- gruntfile.js
+++ gruntfile.js
@@ -1,47 +1,51 @@
 module.exports = function(grunt) {
-// Load Grunt tasks declared in the package.json file
-require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // Load Grunt tasks declared in the package.json file
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-// Configure Grunt
-grunt.initConfig({
+  // Configure Grunt
+  grunt.initConfig({
 
-// Grunt express - our webserver
-// https://github.com/blai/grunt-express
-express: {
-    all: {
+    // run jshint
+    jshint: {
+      files: {
+        src: ['src/**/*.js']
+      }
+    },
+
+    // https://github.com/blai/grunt-express
+    express: {
+      all: {
         options: {
-            bases: ['C:\\Users\\Oskar\\gitrepos\\falling'],
-            port: 8080,
-            hostname: "0.0.0.0",
-            livereload: true
+          bases: ['C:\\Users\\Oskar\\gitrepos\\skeleton'],
+          port: 8080,
+          hostname: "0.0.0.0",
+          livereload: true
         }
-    }
-},
+      }
+    },
 
-// grunt-watch will monitor the projects files
-// https://github.com/gruntjs/grunt-contrib-watch
-watch: {
-    all: {
-            files: ['**/*.html', 'src/**/*.js'],
-            options: {
-                livereload: true
-        }
-    }
-},
-
-// grunt-open will open your browser at the project's URL
-// https://www.npmjs.org/package/grunt-open
-open: {
-    all: {
+    // https://www.npmjs.org/package/grunt-open
+    open: {
+      all: {
         path: 'http://localhost:8080/index.html'
-    }
-}
-});
+      }
+    },
 
-// Creates the `server` task
-grunt.registerTask('server', [
+    // https://github.com/gruntjs/grunt-contrib-watch
+    watch: {
+      all: {
+        files: ['**/*.html', 'src/**/*.js'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'jshint',
     'express',
     'open',
     'watch'
-    ]);
+  ]);
 };
